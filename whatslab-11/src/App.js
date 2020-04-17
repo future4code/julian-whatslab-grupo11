eef32d27b0bfa6ba8e81fa3358bdd2df1fab4bad
Juliana <juliana.berdeville@gmail.com>
2020-04-17 15:34:35 -0300

    Adicionando arquivos do projeto.

--- whatslab-11/src/App.js
+++ whatslab-11/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {mensagem} from './components/mensagem'
 
 function App() {
-  return (
-    <div>
-    </div>
+  return 
+  (
+    <div> {/*DIV do quadradão*/}
+      <div> {/*DIV dos inputs e botao*/}
+      <mensagem/>
+      </div> {/*DIV dos inputs e botao*/}
+    </div> 
   );
 }
 
