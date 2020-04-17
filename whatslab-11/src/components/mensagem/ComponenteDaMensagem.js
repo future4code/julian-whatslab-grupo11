41a907ff9e9fe70fc228b43714ed52fe5a2721b1
Kalleby Gomes <kallebyfg@gmail.com>
2020-04-17 17:51:25 -0300

    Atualizando

--- whatslab-11/src/components/mensagem/ComponenteDaMensagem.js
+++ whatslab-11/src/components/mensagem/ComponenteDaMensagem.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const InputStyle = styled.input`
-    width: 70%;
-    height: 100%;
-    border: 1px solid red;
-`
+
 
 export class ComponenteDaMensagem extends React.Component 
 {
@@ -26,11 +22,6 @@ export class ComponenteDaMensagem extends React.Component
     render () {
     return (
 
-            <div>
-                <InputStyle placeholder={"mensagem"} onChange={this.onChangeValorInputMensagem} 
-                value={this.state.valorInputMensagem}/>
-            </div>
-        
             )
     }
 }
