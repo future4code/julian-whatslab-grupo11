3e71a859c164dbef4d0343bbb8645eeae85a1b41
Juliana <juliana.berdeville@gmail.com>
2020-04-17 18:36:14 -0300

    Adicionando arquivos ao projeto.

--- whatslab-11/src/App.js
+++ whatslab-11/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ComponenteDaMensagem} from './components/mensagem/ComponenteDaMensagem.js'
+import ComponenteDaMensagem from './components/mensagem'
 import styled from 'styled-components'
 
 /*Estilização com styled-components*/
@@ -10,27 +10,6 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
 `
-
-const Quadradao = styled.div`
-  border: 1px solid black;
-  width: 400px;
-  height: 99vh;
-  display: flex;
-  align-items: flex-end;
-`
-const DivMsg = styled.div`
-  width: 100%;
-  height: 30px;
-`
-
-const Container = styled.div`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
 const Quadradao = styled.div`
   border: 1px solid black;
   width: 400px;
@@ -57,7 +36,7 @@ function App() {
           {/*Chamada do componente correspondente à lista de usuários*/}
 
           {/*Chamada do componente correspondente ao input de mensagens*/}
-          <ComponenteDaMensagem/>  
+          <ComponenteDaMensagem />  
           {/*Chamada do componente correspondente ao input de mensagens*/}
 
           {/*Chamada do componente correspondente ao botão de enviar*/}
