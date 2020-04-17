9667093b068064b0644fda5552e45feeb46bae86
Kalleby Gomes <kallebyfg@gmail.com>
2020-04-17 16:59:01 -0300

    Projeto WhatsLab-11

--- whatslab-11/src/App.js
+++ whatslab-11/src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {mensagem} from './components/mensagem'
+import {ComponenteDaMensagem} from './components/mensagem/ComponenteDaMensagem.js'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Quadradao = styled.div`
+  border: 1px solid black;
+  width: 400px;
+  height: 99vh;
+  display: flex;
+  align-items: flex-end;
+`
+const DivMsg = styled.div`
+  width: 100%;
+  height: 30px;
+`
+
 
 function App() {
-  return 
-  (
-    <div> {/*DIV do quadradão*/}
-      <div> {/*DIV dos inputs e botao*/}
-      <mensagem/>
-      </div> {/*DIV dos inputs e botao*/}
-    </div> 
+  return (
+    <Container>
+
+      <Quadradao>{/*DIV do quadradão*/}   
+        <DivMsg> {/*DIV dos inputs e botao*/}
+          {/* <componenteUsuario> </componenteUsuario> */}
+          <ComponenteDaMensagem />  
+          <button></button>
+
+        </DivMsg> {/*DIV dos inputs e botao*/}
+      </Quadradao>
+
+    </Container> 
   );
 }
 
