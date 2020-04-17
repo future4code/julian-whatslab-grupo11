d7cfff03698093a2f2c8f771a138ff2df5ff462d
Kalleby Gomes <kallebyfg@gmail.com>
2020-04-17 17:52:14 -0300

    WhatsLab11

--- whatslab-11/src/App.js
+++ whatslab-11/src/App.js
@@ -3,13 +3,41 @@ import {ComponenteDaMensagem} from './components/mensagem/ComponenteDaMensagem.j
 import styled from 'styled-components'
 
 
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
+const Quadradao = styled.div`
+  border: 1px solid black;
+  width: 400px;
+  height: 99vh;
+  display: flex;
+  align-items: flex-end;
+`
+const DivMsg = styled.div`
+  width: 100%;
+  height: 30px;
 `
 
 
 function App() {
   return (
+    <Container>
 
+      <Quadradao>{/*DIV do quadradão*/}   
+        <DivMsg> {/*DIV dos inputs e botao*/}
+          {/* <componenteUsuario> </componenteUsuario> */}
+          <ComponenteDaMensagem />  
+          <button></button>
+
+        </DivMsg> {/*DIV dos inputs e botao*/}
+      </Quadradao>
+
+    </Container> 
   );
 }
 
